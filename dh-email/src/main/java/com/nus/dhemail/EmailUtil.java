734faf5f5b3fffca9749162283d217ab1f4dfd96
Callmepapa <shihaizhou1104@outlook.com>
2023-10-31 21:23:20 +0800

    email format.

--- dh-email/src/main/java/com/nus/dhemail/EmailUtil.java
+++ dh-email/src/main/java/com/nus/dhemail/EmailUtil.java
@@ -36,7 +36,15 @@ public class EmailUtil {
       mimeMessageHelper.setFrom(from);
       mimeMessageHelper.setTo(emailContent.getEmail());
       mimeMessageHelper.setSubject("LowestPrice Update for " + emailContent.getProductName());
-      mimeMessageHelper.setText("The newLowestPrice for " + emailContent.getProductName() + " has been updated to " + emailContent.getPrice(),true);
+      mimeMessageHelper.setText(
+              "Dear Customer,\n\n" +
+                      "We're thrilled to share some fantastic news with you! " +
+                      "The product '" + emailContent.getProductName() + "' you've subscribed to has just hit a new lowest price.\n\n" +
+                      "Now is the perfect time to grab this amazing deal and make your purchase.\n\n " +
+                      "The new lowest price for " + emailContent.getProductName() + " has been updated to $" + emailContent.getPrice() + ".\n\n" +
+                      "Don't miss out on the savings! Visit our website to explore the latest pricing and secure your order.\n\n" +
+                      "Thank you for being a valued subscriber. We hope you enjoy your shopping experience with us!\n\n" +
+                      "Best Regards,\n[Deal hunter]", true);
       javaMailSender.send(mimeMessageHelper.getMimeMessage());
     } catch (Exception e) {
       return false;
